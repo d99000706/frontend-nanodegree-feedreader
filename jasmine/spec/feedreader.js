4b4cb95f973ff39ddfc48af2ef69d975278b6f5f
d99000706 <d99000706@gmail.com>
2015-05-31 07:59:30 -0400

    modified indentation and comments in feedreader.js per reviewer's comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -28,49 +28,50 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* 'have urls' is a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it('have urls', function() {
+        it('have urls', function() {
             // loop thru each feed and check for url defined & length > 0
-            for (var i=0; i < allFeeds.length; i++) {
+            for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].url).toBeDefined();
                 expect(allFeeds[i].url.length).not.toBe(0);
             }
-         });
+        });
 
-        /* TODO: Write a test that loops through each feed
+        /* 'have names' is a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-         it('have names', function() {
+        it('have names', function() {
             // loop thru each feed and check for name defined & length > 0
-            for (var i=0; i < allFeeds.length; i++) {
+            for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeDefined();
                 expect(allFeeds[i].name.length).not.toBe(0);
             }
-         });
+        });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* 'The menu' is a test suite to verify operation of the user menu */
     describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* 'is hidden by default' is a test that ensures the menu element is
+         * hidden by default. We use jquery and check for a certain css class
+         * responsible for hiding of the menu element.
          */         
         it('is hidden by default', function() {
             // by default we expect the menu to be hidden
             expect( $('body').hasClass('menu-hidden')).toBe(true);
         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
+         /* 'displays when clicked and hides when clicked again' is a test that ensures the menu changes
+          * visibility when the menu icon is clicked. This test has
+          * two expectations: does the menu display when
           * clicked and does it hide when clicked again.
+          * Again, jquery is used to check for a certain css classes
+          * responsible for hiding and showing of the menu element.
           */
         it('displays when clicked and hides when clicked again', function() {
             // first click we expect the menu to NOT be hidden
@@ -83,13 +84,15 @@ $(function() {
         });
     });
     
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* 'Initial Entries' is a test suite that verifies feed data coming from
+     * the Google Feed Reader service.
+     */
     describe('Initial Entries', function() {
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* 'are loaded' is a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
+         * Note that loadFeed() is asynchronous so this test requires
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
          
@@ -111,19 +114,21 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* "New Feed Selection" is a test suite to check that the feed data changes 
+     * when a new feed is loaded
+     */
     describe('New Feed Selection', function() {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* 'feed content is changed' is a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        // since loadFeed is asynchronous, get it with a beforeEach call using done
+        // since loadFeed is asynchronous, start it within a beforeEach call using done
         // we want to check if feeds change so use a loop & array for a couple of feed id's
         var contentList = [];
         var numFeeds = 2;
 
-        for (var i=0; i < numFeeds; i++) {
+        for (var i = 0; i < numFeeds; i++) {
 
             //use an iffe to capture the loop variable, feed id
             ( function(feedId) {
